c617556abdf68e76386940980e4a2ce42d363eea
Brian Krisler <bkrisler@bkds.com>
2023-12-04 21:18:00 -0500

    Day 3, finally settled on a parsing solution. Had a few other stupid bugs but finally got it working!

--- 2023/03_gear_ratios/aoc202303.py
+++ 2023/03_gear_ratios/aoc202303.py
@@ -10,26 +10,18 @@ from io import StringIO
 import numpy as np
 
 
-def find_adj(matrix, x, y, d=1):
-    # Create the nearest neighbors
-    x_idx, y_idx = np.mgrid[-d:d + 1, -d:d + 1]
-
-    # Remove the center (0,0) index
-    center = (x_idx == 0) & (y_idx == 0)
-    x_idx = x_idx[~center]
-    y_idx = y_idx[~center]
-    #print("fa: {}, {}".format(x_idx+x, y_idx+y))
-    res = matrix[x_idx+x, y_idx+y]
-
-    #print("Pos: {} has adjacent: {}".format(matrix[x, y], res))
-
-    return res
-
-
 def parse_data_np(puzzle_input):
     """Parse input."""
     matrix = np.genfromtxt(StringIO(puzzle_input), dtype='str', delimiter=1, deletechars=None, comments=None)
     return matrix
+
+
+def parse_symbol_list(puzzle_input):
+    symbols = []
+    for c in puzzle_input:
+        if c != '.' and c != '\n' and not c.isdigit() and c not in symbols:
+            symbols.append(c)
+    return symbols
 
 
 def parse_data(puzzle_input):
@@ -39,47 +31,46 @@ def parse_data(puzzle_input):
         matches = re.finditer(pattern, row)
         for match in matches:
             #print("{}: [{}, ({}, {})]".format(match.group(), idx, match.start(), match.end()))
-            result[match.group()].append(idx)
-            result[match.group()].append(match.span())
+            key = (idx, match.start())
+            result[key].append(int(match.group()))
+            result[key].append(match.end()-match.start())
 
-    return result, parse_data_np(puzzle_input)
+    symbols = parse_symbol_list(puzzle_input)
 
-
-def find_cells_near_symbol(data):
-    symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=']
-    mask = np.isin(data, symbols)
-    for x, y in zip(*np.where(mask)):
-        adj = find_adj(data, x, y)
-        for idx, v in enumerate(adj):
-            if v != '.':
-                print(idx, v, data[x, y])
+    return result, symbols, parse_data_np(puzzle_input)
 
 
-def has_adjacent(matrix, x_coord, y_coord):
-    symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=']
+def has_adjacent(matrix, symbols, x_coord, y_coord):
 
     adjacency = [(i, j) for i in (-1, 0, 1) for j in (-1, 0, 1) if not (i == j == 0)]
     for dx, dy in adjacency:
         if 0 <= (x_coord + dx) < np.shape(matrix)[1] and 0 <= y_coord + dy < np.shape(matrix)[1]:
             cv = matrix[x_coord + dx, y_coord + dy]
+            #print("Check: {} and {}, {}".format(cv, x_coord + dx, y_coord + dy))
             if cv in symbols:
                 return True
 
     return False
 
 
-def part1(data, matrix):
+def part1(data, symbols, matrix):
     """Solve part 1."""
-
+    print(data)
     matches = []
-    for k, v in data.items():
-        for x in range(v[1][0], v[1][1]):
-            if has_adjacent(matrix, v[0], x):
-                k_int = int(k)
-                if k_int not in matches:
-                    matches.append(k_int)
+    for pos, part in data.items():
+        has_match = False
+        for x in range(pos[1], pos[1]+part[1]):
+            #print("{} at {}, {}".format(part[0], pos[0], x))
+            if has_adjacent(matrix, symbols, pos[0], x):
+                print("Match: {}".format(part[0]))
+                has_match = True
+        if has_match:
+            matches.append(part[0])
 
+    print('--- Part 1 ----')
     print(sum(matches))
+    print('---')
+
 
 
 def part2(data):
@@ -88,8 +79,8 @@ def part2(data):
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
-    data, matrix = parse_data(puzzle_input)
-    yield part1(data, matrix)
+    data, symbols, matrix = parse_data(puzzle_input)
+    yield part1(data, symbols, matrix)
     yield part2(data)
 
 
