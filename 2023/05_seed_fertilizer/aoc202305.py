330659060e8f89a53271015b05decc1bb01ad267
Brian Krisler <bkrisler@bkds.com>
2023-12-05 21:51:41 -0500

    Part 2 is taking hours. Something is off on the approach. This attempt used processes, but backing that out.

--- 2023/05_seed_fertilizer/aoc202305.py
+++ 2023/05_seed_fertilizer/aoc202305.py
@@ -1,4 +1,5 @@
 """AoC 5, 2023: Seed Fertilizer."""
+import concurrent.futures
 
 # Standard library imports
 import pathlib
@@ -81,8 +82,67 @@ def part1(data):
     return min(locations)
 
 
+def get_location(seed, data):
+    # print("  Seed: {}".format(seed))
+    # Get Soil Number
+    soil = get_match(seed, data['seed-to-soil'])
+
+    # Get fertilizer Number
+    fertilizer = get_match(soil, data['soil-to-fertilizer'])
+
+    # Get Water Number
+    water = get_match(fertilizer, data['fertilizer-to-water'])
+
+    # Get Light Number
+    light = get_match(water, data['water-to-light'])
+
+    # Get Temperature Number
+    temp = get_match(light, data['light-to-temperature'])
+
+    # Get Humidity Number
+    humidity = get_match(temp, data['temperature-to-humidity'])
+
+    # Get Location Number
+    location = get_match(humidity, data['humidity-to-location'])
+
+    #print("Seed: {}, Soil: {}, Fertilizer: {}, Water: {}, Light: {}, Temp: {}, Humidity: {} Location: {}".format(seed, soil, fertilizer, water, light, temp, humidity, location))
+    return location
+
+
+def process_entry(entry, data):
+    min_location = sys.maxsize
+
+    for seed in range(entry[0], entry[0] + entry[1]):
+        location = get_location(seed, data)
+        if location < min_location:
+            min_location = location
+            #print("Min = {}".format(min_location))
+    return min_location
+
+
 def part2(data):
     """Solve part 2."""
+    print("\nStart Part 2")
+
+    min_location = sys.maxsize
+    pairs = zip(data['seeds'][::2], data['seeds'][1::2])
+
+    for entry in pairs:
+        m = process_entry(entry, data)
+        if m < min_location:
+            min_location = m
+
+    # executor = concurrent.futures.ProcessPoolExecutor(10)
+    # futures = [executor.submit(process_entry, entry, data) for entry in pairs]
+    # concurrent.futures.wait(futures, return_when=concurrent.futures.ALL_COMPLETED)
+
+    # merged_list = [v for f in futures for v in f.result()]
+    # print(merged_list)
+    # min_location = min(merged_list)
+
+#        print("Processing: {} with {} seeds".format(entry, entry[1]))
+
+    return min_location
 
 
 def solve(puzzle_input):
