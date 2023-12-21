27a630635f735d47ee65fa72d7206e7309cc8282
Brian Krisler <bkrisler@bkds.com>
2023-12-20 22:28:12 -0500

    Fixed a few bugs but still not passing the test.

--- 2023/07_camel_cards/aoc202307.py
+++ 2023/07_camel_cards/aoc202307.py
@@ -35,8 +35,76 @@ def find_type(hand):
     return 0
 
 
-def get_higher(a, b):
-    ranking = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2', '1']
+def find_type_two(hand):
+    result = 0
+
+    res = Counter(hand)
+    if 5 in res.values():
+        # Five of a kind
+        result = 6
+    elif 4 in res.values():
+        # Four of a kind
+        if 'J' in hand and int(res['J']) == 1:
+            # KKKKJ == KKKKK
+            result = 6
+            #print("{} is now 5 of a kind. Type {}".format(hand, result))
+        else:
+            result = 5
+    elif 3 in res.values() and 2 in res.values():
+        # Full house -- 3 of a kind and a pair
+        if 'J' in hand:
+            # It becomes 5 of a kind. KKKJJ (KKKKK) or JJJKK (KKKKK)
+            result = 6
+            #print("{} is now 5 of a kind. Type {}".format(hand, result))
+        else:
+            result = 4
+    elif 3 in res.values():
+        # Three of a kind
+        if 'J' in res.keys():
+            # Options: KKKJA (KKKKA) or JJJKA (KKKKA). Becomes 4 of a kind.
+            result = 5
+            #print("{} is now 4 of a kind. Type {}".format(hand, result))
+        else:
+            result = 3
+    elif len([x for x in res.values() if x == 2]) == 2:
+        # Two Pair
+        if 'J' in res.keys():
+            # Options: JJAA3 (AAAA3) or AAKKJ (AAAKK)
+            if int(res['J']) == 2:
+                # Becomes 4 of a kind
+                result = 5
+                #print("{} is now 4 of a kind. Type {}".format(hand, result))
+            else:
+                # Becomes Full House
+                result = 4
+                #print("{} is now Full House. Type {}".format(hand, result))
+        else:
+            result = 2
+    elif len([x for x in res.values() if x == 2]) == 1:
+        # One Pair
+        if 'J' in res.keys():
+            # JJKAQ (AAAKQ) or AAKQJ (AAAKQ). Becomes 3 of a kind
+            result = 3
+            #print("{} is now 3 of a kind. Type {}".format(hand, result))
+        else:
+            result = 2
+    else:
+        if 'J' in res.keys():
+            result = 2
+        else:
+            result = 1
+        #print("{} is now 1 pair. Type {}".format(hand, result))
+
+    #print("{} is type: {}".format(hand, result))
+    return result
+
+
+def get_higher(a, b, part=1):
+    if part == 2:
+        ranking = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J']
+    else:
+        ranking = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+
     if ranking.index(a) < ranking.index(b):
         return a
     else:
@@ -64,9 +132,36 @@ def second_order(hand1, hand2):
     return result
 
 
+def second_order_p2(hand1, hand2):
+    t1 = find_type_two(hand1)
+    t2 = find_type_two(hand2)
+    result = 0
+    if t1 == t2:
+        for x in range(5):
+            if hand1[x] != hand2[x]:
+                higher = get_higher(hand1[x], hand2[x], 2)
+                if hand1[x] == higher:
+                    result = 1
+                    break
+                else:
+                    result = -1
+                    break
+    else:
+        result = 0
+
+    #print("Compare: {} with {} == {}".format(hand1, hand2, result))
+    return result
+
+
 def rank_sort(hand):
     phase_1 = sorted(hand, key=find_type)
     phase_2= sorted(phase_1, key=functools.cmp_to_key(second_order))
+    return phase_2
+
+
+def rank_sort_p2(hand):
+    phase_1 = sorted(hand, key=find_type_two)
+    phase_2= sorted(phase_1, key=functools.cmp_to_key(second_order_p2))
     return phase_2
 
 
@@ -85,7 +180,24 @@ def part1(data):
 
 
 def part2(data):
-    """Solve part 2."""
+    """Solve part 2.
+    First Attempt:  250795606 -- Too High
+    Second Attempt: 250769877 -- Too High
+    Third Attempt:  250600944 -- Too High
+    Fourth Attempt: 249541401 -- Incorrect
+    """
+    hand = [x[0] for x in data]
+    bids = dict(data)
+    ranked = rank_sort_p2(hand)
+    winnings = 0
+    print()
+    for idx, hand in enumerate(ranked, 1):
+        bid = int(bids[hand]) * idx
+        print("{} has rank {} and bid: {}".format(hand, idx, bid))
+        winnings += bid
+
+    print()
+    return winnings
 
 
 def solve(puzzle_input):
