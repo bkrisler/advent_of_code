1058e1de7f591df271e499879217d01b1e68aeca
Brian Krisler <bkrisler@bkds.com>
2023-12-23 22:33:17 -0500

    Day 8, Part two. Read up on reddit about the LCM trick.

--- 2023/08_haunted_wasteland/aoc202308.py
+++ 2023/08_haunted_wasteland/aoc202308.py
@@ -5,6 +5,7 @@ import pathlib
 import re
 import sys
 from collections import defaultdict
+from math import lcm
 
 
 def parse_data(puzzle_input):
@@ -41,9 +42,74 @@ def part1(data):
     return steps
 
 
-def part2(data):
-    """Solve part 2."""
+def part2_orig(data):
+    """Solve part 2.
+    Attempt One: 913793 is too low
+    """
+    print()
+    nav = [*data['navigate']]
+    next_nodes = [x for x in data.keys() if x[-1] == 'A']
+    sz = len(nav)
+    steps = idx = 0
+    loops = defaultdict(list)
+    check = None
+    sep = 0
+    turns = []
+    while idx <= sz:
+        next_nodes = [data[entry][nav[idx]] for entry in next_nodes]
+        turns.append(nav[idx])
+        if check is None:
+            check = next_nodes[0]
+        for ix, n in enumerate(next_nodes):
+            if n == check and n in loops[ix]:
+                print("{}/{} - {}: {}".format(check, loops[ix][-1], "".join(turns), sep))
+                sep = 0
+                turns = []
+            else:
+                sep += 1
+            loops[ix].append(n)
+        steps += 1
+        idx += 1
+        end = [entry[-1] for entry in next_nodes]
+        end_same = all(c == end[0] for c in end)
+        #print("Step: {}, End: {}".format(steps, end))
+        if end_same and end[0] == 'Z':
+            break
+        if idx == sz:
+            #print(loops)
+            idx = 0
 
+    return steps
+
+
+def part2(data):
+    """Solve part 2.
+    Attempt One: 913793 is too low
+    """
+    print()
+    nav = [*data['navigate']]
+    next_nodes = [x for x in data.keys() if x[-1] == 'A']
+    node_a = next_nodes[0]
+    sz = len(nav)
+    steps = idx = 0
+    paths = {}
+    while idx <= sz:
+        next_nodes = [data[entry][nav[idx]] for entry in next_nodes]
+        steps += 1
+        idx += 1
+        #print("Step: {}, End: {}".format(steps, end))
+        for x, n in enumerate(next_nodes):
+            if n[-1] == 'Z':
+                paths[x] = steps
+                if len(paths.keys()) == len(next_nodes):
+                    vv = list(paths.values())
+                    result = lcm(*vv)
+                    return result
+        if idx == sz:
+            #print(loops)
+            idx = 0
+
+    return steps
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
--- 2023/08_haunted_wasteland/test_aoc202308.py
+++ 2023/08_haunted_wasteland/test_aoc202308.py
@@ -22,6 +22,12 @@ def example2():
     return aoc202308.parse_data(puzzle_input)
 
 
+@pytest.fixture
+def example3():
+    puzzle_input = (PUZZLE_DIR / "example3.txt").read_text().rstrip()
+    return aoc202308.parse_data(puzzle_input)
+
+
 def test_parse_example1(example1):
     """Test that input is parsed properly."""
     assert example1
@@ -40,10 +46,9 @@ def test_part1_example2(example2):
     assert aoc202308.part1(example2) == 6
 
 
-@pytest.mark.skip(reason="Not implemented")
-def test_part2_example1(example1):
+def test_part2_example3(example3):
     """Test part 2 on example input."""
-    assert aoc202308.part2(example1) == ...
+    assert aoc202308.part2(example3) == 6
 
 
 @pytest.mark.skip(reason="Not implemented")
