67049168576bf811386fb50846fa009683b47750
Brian Krisler <bkrisler@bkds.com>
2024-12-13 23:11:58 -0500

    Day 9, part 1 works

--- 2024/09_disk_fragmenter/aoc202409.py
+++ 2024/09_disk_fragmenter/aoc202409.py
@@ -2,16 +2,82 @@
 
 # Standard library imports
 import pathlib
+import re
 import sys
 
 
+class DiskMap(object):
+    def __init__(self, raw):
+        self.raw = raw
+        self.id_map = {}
+        self.map = []
+        self.build_map()
+
+        for idx, f in enumerate(self.raw[::2]):
+            self.id_map[idx] = f
+
+    def build_map(self):
+        freespace = self.raw[1::2]
+        for idx, x in enumerate(self.raw[::2]):
+            for _ in range(int(x)):
+                self.map.append(str(idx))
+            if idx < len(freespace):
+                for _ in range(int(freespace[idx])):
+                    self.map.append('.')
+
+
+    def advance_end(self, dmap, idx):
+        end_ptr = idx
+        while dmap[end_ptr] == '.':
+            end_ptr = end_ptr - 1
+
+        return end_ptr
+
+    def defrag(self):
+        new_map = self.map.copy()
+        # free_size = sum([int(x) for x in self.raw[1::2]])
+        # total_size = len(self.map)
+
+        end_ptr = len(self.map) - 1
+        while new_map[end_ptr] == '.':
+            end_ptr = end_ptr - 1
+
+        for idx, v in enumerate(new_map):
+            if idx >= end_ptr:
+                break
+
+            if v == '.':
+                new_map[idx] = new_map[end_ptr]
+                new_map[end_ptr] = '.'
+                end_ptr = self.advance_end(new_map, end_ptr)
+
+        return new_map
+
+
+    def display(self):
+        print('\n')
+        self.defrag()
+        return ''.join(self.map)
+
+
+def checksum(entry):
+    result = 0
+    for idx, v in enumerate(entry):
+        if v == '.':
+            v = 0
+        result += idx * int(v)
+    return result
+
 def parse_data(puzzle_input):
     """Parse input."""
-
+    dm = DiskMap(puzzle_input)
+    dm.display()
+    return dm
 
 def part1(data):
     """Solve part 1."""
-
+    result = data.defrag()
+    return checksum(result)
 
 def part2(data):
     """Solve part 2."""
--- 2024/09_disk_fragmenter/test_aoc202409.py
+++ 2024/09_disk_fragmenter/test_aoc202409.py
@@ -22,16 +22,18 @@ def example2():
     return aoc202409.parse_data(puzzle_input)
 
 
-@pytest.mark.skip(reason="Not implemented")
+#@pytest.mark.skip(reason="Not implemented")
 def test_parse_example1(example1):
     """Test that input is parsed properly."""
-    assert example1 == ...
+    assert isinstance(example1, aoc202409.DiskMap)
+    assert example1.raw is not None
+    assert len(example1.raw) == 19
+    assert example1.display() == '00...111...2...333.44.5555.6666.777.888899'
 
-
-@pytest.mark.skip(reason="Not implemented")
+#@pytest.mark.skip(reason="Not implemented")
 def test_part1_example1(example1):
     """Test part 1 on example input."""
-    assert aoc202409.part1(example1) == ...
+    assert aoc202409.part1(example1) == 1928
 
 
 @pytest.mark.skip(reason="Not implemented")
